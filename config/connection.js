2ae3ad5bb34f1bd51baf103c070f82ea35e1dc04
srishtie <srishtie.tech@gmail.com>
2021-04-19 18:34:17 +0530

    create index, dumb collection

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 const elasticsearch = require("elasticsearch");
+const cities = require('../assets/cities.json');
 const client = new elasticsearch.Client({
   hosts: ["http://localhost:9200"],
 });
@@ -14,4 +15,34 @@ client.ping(
     }
   }
 );
+
+// create a new index called scotch.io-tutorial. If the index has already been created, this function fails safely
+// client.indices.create({
+//   index: 'elastic-demo'
+// }, (error, response, status) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("created a new index", response);
+//   }
+// });
+
+let bulk = [];
+cities.forEach(city => {
+  bulk.push({
+    index: {
+      _index: "elastic-demo",
+      _type: "cities_list",
+    }
+  })
+  bulk.push(city)
+})
+//perform bulk indexing of the data passed
+client.bulk({ body: bulk }, (err, response) => {
+  if (err) {
+    console.log("Failed Bulk operation", err)
+  } else {
+    console.log("Successfully imported %s", cities.length);
+  }
+});
 module.exports = client;
